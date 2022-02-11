a58dd64ddf0b3961c79f69cfbf9366d03b85979c
Vitaliy Demidov <vitaliy@scalr.com>
2022-02-11 15:29:06 -0800

    build linux/aarch64 wheels

--- pre-build-command.sh
+++ pre-build-command.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
 
-curl https://storage.googleapis.com/golang/go1.13.linux-amd64.tar.gz --silent --location | tar -xz
+ARCH=$(uname -m)
+curl "https://storage.googleapis.com/golang/go1.13.linux-${ARCH}.tar.gz" --silent --location | tar -xz
 
 export PATH="$(pwd)/go/bin:$PATH"
