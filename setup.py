004d574d59142749bb2fabf4569b3dac3b69233d
Anton Agestam <git@antonagestam.se>
2022-10-30 23:23:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,6 +24,8 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         "License :: OSI Approved :: MIT License",
     ],
     packages=find_packages(),
