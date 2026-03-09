fc4664ddf9c5982080dc55519b62d29fa1286e6e
Vitaliy Demidov <vitaliy@scalr.com>
2026-03-08 17:16:46 -0700

    CFFI_DLLEXPORT macros

--- pygohcl/build_cffi.py
+++ pygohcl/build_cffi.py
@@ -11,24 +11,16 @@ ffi.set_source(
 )
 
 ffi.cdef(
+    """        
+    typedef struct {
+        char *json;
+        char *err;
+    } parseResponse;
+
+    parseResponse Parse(char* a, int keepInterpFlag);
+    parseResponse ParseAttributes(char* a);
+    char* EvalValidationRule(char* c, char* e, char* n, char* v);
+    void free(void *ptr);
     """
-        #if defined(_WIN32)
-        #  define CFFI_DLLEXPORT  __declspec(dllexport)
-        #elif defined(__GNUC__)
-        #  define CFFI_DLLEXPORT  __attribute__((visibility("default")))
-        #else
-        #  define CFFI_DLLEXPORT  /* nothing */
-        #endif
-        
-        typedef struct {
-            char *json;
-            char *err;
-        } parseResponse;
- 
-        parseResponse Parse(char* a, int keepInterpFlag);
-        parseResponse ParseAttributes(char* a);
-        char* EvalValidationRule(char* c, char* e, char* n, char* v);
-        CFFI_DLLEXPORT void free(void *ptr);
-        """
 )
 ffi.compile()
